0e76f00a1874c86e90ce0e54329c3a44d9c2ba68
Niraj Markandey <niraj.markandey@bluejeansnet.com>
2018-05-26 18:04:45 +0530

    Ignoring .git files in gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const apidoc = require("gulp-apidoc");
 
 const paths = {
     tocopy: ["./**", "!./bin", "!./bin/**", "!./**/*.ts", "!./.vscode", "!./.vscode/**", "!./gulpfile.js", "!./*.log", "!./*.lock", "!./*.md", "!./*.json", "!./*.yml", "!./LICENSE",
-        "!./test/unit/*.ts", "!./test/mocha.opts", "!./coverage", "!./coverage/**", "!./routes/**apidoc**", "!./tasks", "!./tasks/**", "!./node_modules", "!./node_modules/**", "!./dist", "!./dist/**"
+        "!./test/unit/*.ts", "!./test/mocha.opts", "!./coverage", "!./coverage/**", "!./routes/**apidoc**", "!./tasks", "!./tasks/**", "!./node_modules", "!./node_modules/**", "!./dist", "!./dist/**","!./.git","!./.git/**","!./bin/.git","!./bin/.git/**"
     ],
     tsfiles: ["./config", "./controllers", "./models", "./routes/*.ts", "./test"]
 };
